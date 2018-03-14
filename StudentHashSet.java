d0ee76f18096ef3b73750e479889825dc1e1a3c8
Jackafoos <jem319@pitt.edu>
2018-03-13 20:05:50 -0400

    Empty Student HashSet Provided by the Instructor.

---
+++ StudentHashSet.java
@@ -0,0 +1,17 @@
+public class StudentHashSet {
+
+  public void add(Student s) {
+  }
+
+  public boolean remove(Student s) {
+    return null;
+  }
+
+  public boolean contains(Student s) {
+    return false;
+  }
+
+  public int size() {
+    return 0;
+  }
+}
